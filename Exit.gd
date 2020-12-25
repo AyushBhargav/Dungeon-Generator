18552a19b62b07627e2b08051d90939fa67690c9
Ayush Bhargav <ab.ayushbhargav0207@gmail.com>
2020-12-25 21:06:37 +0530

    Player added

---
+++ Exit.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+signal leaving_level
+func _on_Exit_body_entered(body):
+	emit_signal("leaving_level")
--- World.gd
+++ World.gd
@@ -1,16 +1,37 @@
 extends Node2D
 
+const Player = preload("res://Player.tscn")
+const Exit = preload("res://Exit.tscn")
+
 var borders = Rect2(1, 1, 31, 16)
 
 onready var tileMap = $TileMap
 
 func _ready():
+	randomize()
 	generate_level()
 	
 func generate_level():
 	var walker = Walker.new(Vector2(19, 11), borders)
 	var map = walker.walk(200)
+	
+	var player = Player.instance()
+	add_child(player)
+	player.position = map.front() * 32
+	
+	var exit = Exit.instance()
+	add_child(exit)
+	exit.position = walker.get_end_room().position*32
+	exit.connect("leaving_level", self, "reload_level")
+	
 	walker.queue_free()
 	for location in map:
 		tileMap.set_cellv(location, -1)
 	tileMap.update_bitmask_region(borders.position, borders.end)
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		reload_level()
+		
+func reload_level():
+	get_tree().reload_current_scene()
